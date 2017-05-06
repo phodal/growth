ce33ca80c4eb21e39f83c0a171ba56d66c30cf52
goze <383217112@qq.com>
2017-05-06 12:24:39 +0800

    lint

--- index.android.js
+++ index.android.js
@@ -4,32 +4,13 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
-
-export default class growth extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -49,5 +30,20 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const growth = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native!
+        </Text>
+    <Text style={styles.instructions}>
+      To get started, edit index.android.js
+        </Text>
+    <Text style={styles.instructions}>
+      Double tap R on your keyboard to reload,{'\n'}
+      Shake or press menu button for dev menu
+        </Text>
+  </View>
+);
 
 AppRegistry.registerComponent('growth', () => growth);
