a5bed30035260816f879145d689f0dbe847db7ef
travelgeezer <travelgeezer@icloud.com>
2017-05-11 17:22:54 +0800

    create custom web font icon.

---
+++ src/icon/GrowthFont.js
@@ -0,0 +1,15 @@
+
+
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from '../../assetc/glyphmaps/growth.json';
+
+const Icon = createIconSetFromIcoMoon(icoMoonConfig, 'growth');
+
+export default Icon;
+
+export const Button = Icon.Button;
+export const TabBarItem = Icon.TabBarItem;
+export const TabBarItemIOS = Icon.TabBarItemIOS;
+export const ToolbarAndroid = Icon.ToolbarAndroid;
+export const getImageSource = Icon.getImageSource;
+
