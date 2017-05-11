8f773024a37e0947bec675f7c138a1240f312b45
travelgeezer <travelgeezer@icloud.com>
2017-05-11 17:29:27 +0800

    update discover UI.

--- src/containers/discover/Discover.js
+++ src/containers/discover/Discover.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import AppStyle from '../../theme/styles';
+import Icon from '../../icon/GrowthFont';
+import OnLineReadItem from './DiscoverOnlineReadItem';
+import MoreItem from './DiscoverMoreItem';
+
+const home1 = require('../../../assetc/growth-ui/img/home-1.jpg');
+const home2 = require('../../../assetc/growth-ui/img/home-2.jpg');
+const home3 = require('../../../assetc/growth-ui/img/home-3.jpg');
+
+const iconSize = 30;
+
 
 class Discover extends Component {
   static componentName = 'Discover';
 
   render = () => (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <ScrollView style={AppStyle.discoverParentStyle}>
+      <View style={{ height: 300 }}>
+        <View style={AppStyle.discoverTitle}>
+          <Text style={AppStyle.discoverTextColor}>在线资源</Text>
+        </View>
+        <View style={AppStyle.discoverFirstGrid}>
+          <View style={AppStyle.discoverGridFirstItem}>
+            <Icon name="icon-social" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>学习路线</Text>
+          </View>
+          <View style={AppStyle.discoverGridLastItem}>
+            <Icon name="explore-1-2" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>练手项目</Text>
+          </View>
+        </View>
+        <View style={AppStyle.discoverOtherGrid}>
+          <View style={AppStyle.discoverGridFirstItem}>
+            <Icon name="explore-1-3" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>工具箱</Text>
+          </View>
+          <View style={AppStyle.discoverGridLastItem}>
+            <Icon name="explore-1-4" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>在线文章</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ height: 420 }}>
+        <View style={AppStyle.discoverTitle}>
+          <Text style={AppStyle.discoverTextColor}>本地资源</Text>
+        </View>
+        <View style={AppStyle.discoverFirstGrid}>
+          <View style={AppStyle.discoverGridFirstItem}>
+            <Icon name="icon-graowth" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>技能测验</Text>
+          </View>
+          <View style={AppStyle.discoverGridLastItem}>
+            <Icon name="explore-2-2" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>解决方案</Text>
+          </View>
+        </View>
+        <View style={AppStyle.discoverOtherGrid}>
+          <View style={AppStyle.discoverGridFirstItem}>
+            <Icon name="explore-2-3" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>读书路线</Text>
+          </View>
+          <View style={AppStyle.discoverGridLastItem}>
+            <Icon name="explore-2-4" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>书籍列表</Text>
+          </View>
+        </View>
+        <View style={AppStyle.discoverOtherGrid}>
+          <View style={AppStyle.discoverGridFirstItem}>
+            <Icon name="explore-2-5" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>文章列表</Text>
+          </View>
+          <View style={AppStyle.discoverGridLastItem}>
+            <Icon name="explore-2-6" size={iconSize} style={AppStyle.discoverTextColor} />
+            <Text style={AppStyle.discoverGrideItem}>待办事项</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ height: 340 }}>
+        <View style={AppStyle.discoverTitle} >
+          <Text style={AppStyle.discoverTextColor}>在线阅读</Text>
+        </View>
+        <OnLineReadItem image={home1} imageFlex={7} title="《Growth: 全栈增长工程师实战》" titleFlex={3} />
+        <OnLineReadItem marginTop={15} image={home2} imageFlex={7} title="《Ideabock: 练手项目集》" titleFlex={3} />
+      </View>
+      <View style={{ height: 190 }}>
+        <View style={AppStyle.discoverTitle} >
+          <Text style={AppStyle.discoverTextColor}>更多</Text>
+        </View>
+        <MoreItem image={home3} title="你还需要什么功能？" titleBackgroundColor="rgba(255,255,255, .5)" />
+      </View>
+    </ScrollView>
   );
 }
 
