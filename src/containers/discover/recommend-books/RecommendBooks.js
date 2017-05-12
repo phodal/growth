3c4d0a83dbb2b040c075e69e059acb55f048a412
travelgeezer <travelgeezer@icloud.com>
2017-05-12 17:01:02 +0800

    [Discover] detail: create recommend-books.

---
+++ src/containers/discover/recommend-books/RecommendBooks.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class RecommendBooks extends Component {
+  static componentName = 'RecommendBooks';
+  render = () => (
+    <Text>RecommendBooks</Text>
+  );
+}
+export default RecommendBooks;
