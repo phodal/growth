98792cd1fdbba90bbb59d232bd2574de73af384c
travelgeezer <travelgeezer@icloud.com>
2017-05-12 16:59:41 +0800

    [Discover] detail: create chapter list.

---
+++ src/containers/discover/chapter-list/ChapterList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class ChapterList extends Component {
+  static componentName = 'ChapterList';
+  render = () => (
+    <Text>ChapterList</Text>
+  );
+}
+export default ChapterList;
