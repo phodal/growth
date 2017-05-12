52cda5efc29edb0b5bbe4c929515167061f2e857
travelgeezer <travelgeezer@icloud.com>
2017-05-12 17:02:42 +0800

    [Discover] detail: create toolbox list.

---
+++ src/containers/discover/toolbox-list/ToolBoxList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class ToolBoxList extends Component {
+  static componentName = 'ToolBoxList';
+  render = () => (
+    <Text>ToolBoxList</Text>
+  );
+}
+export default ToolBoxList;
