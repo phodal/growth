3d1dcd59461789dd1c36bcda044d7dd3b36a6055
travelgeezer <travelgeezer@icloud.com>
2017-05-12 17:12:46 +0800

    [Discover] lanuch: create lanuch & add discover item jump event.

---
+++ src/containers/discover/Launch.js
@@ -0,0 +1,42 @@
+import { Actions } from 'react-native-router-flux';
+
+const launch = {
+  roadmapList: () => {
+    Actions.roadmapList();
+  },
+  projectList: () => {
+    Actions.projectList();
+  },
+  toolBox: () => {
+    Actions.toolBoxList();
+  },
+  articleList: () => {
+    Actions.articleList();
+  },
+  examList: () => {
+    Actions.examList();
+  },
+  solution: () => {
+    Actions.solution();
+  },
+  thoughtworksBooks: () => {
+    Actions.thoughtworksBooks();
+  },
+  recommendBooks: () => {
+    Actions.recommendBooks();
+  },
+  recommendArticles: () => {
+    Actions.recommendArticles();
+  },
+  todoLists: () => {
+    Actions.todoLists();
+  },
+  chapterList: () => {
+    Actions.chapterList();
+  },
+  openGitHub: (url) => {
+    Actions.growthWebView({ link: url });
+  },
+};
+
+export default launch;
