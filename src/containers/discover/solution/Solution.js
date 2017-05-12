7b5d95e71f5cacedb040bd6ef0b2896bc9d1cd19
travelgeezer <travelgeezer@icloud.com>
2017-05-12 17:01:37 +0800

    [Discover] detail: create solution.

---
+++ src/containers/discover/solution/Solution.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Solution extends Component {
+  static componentName = 'Solution';
+  render = () => (
+    <Text>Solution</Text>
+  );
+}
+export default Solution;
