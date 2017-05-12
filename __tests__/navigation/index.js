4259107099c0926a94e4450d7005c2723c7b9cb4
Phodal HUANG <h@phodal.com>
2017-05-12 22:40:49 +0800

    [T] test: add test for navigation

---
+++ __tests__/navigation/index.js
@@ -0,0 +1,22 @@
+import 'react-native';
+import React from 'react';
+import { expect } from 'chai';
+import renderer from 'react-test-renderer';
+
+import scenes from '../../src/navigation/index';
+
+describe('router test', () => {
+  it('test router should currently', () => {
+    expect(scenes.comingSoon.component.name).to.equal('ComingSoonView');
+  });
+
+  it('should return correct tabs key', () => {
+    const tabs = scenes.tabs.children;
+
+    expect(tabs[0]).to.eq('home');
+    expect(tabs[1]).to.eq('discover');
+    expect(tabs[2]).to.eq('skillTree');
+    expect(tabs[3]).to.eq('community');
+    expect(tabs[4]).to.eq('userCenter');
+  });
+});
