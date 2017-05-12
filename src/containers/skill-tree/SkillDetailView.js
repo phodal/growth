865b286cba204161d8e94427c0ec13155019b620
Phodal HUANG <h@phodal.com>
2017-05-12 14:03:06 +0800

    [SkillTree] detail: create book list

--- src/containers/skill-tree/SkillDetailView.js
+++ src/containers/skill-tree/SkillDetailView.js
@@ -1,10 +1,10 @@
-import React, { Component, PropTypes } from 'react';
-import { Text, View } from 'react-native';
-import { Card, List, ListItem } from 'react-native-elements';
+import React, {Component, PropTypes} from 'react';
+import {Text, View} from 'react-native';
+import {Card, List, ListItem} from 'react-native-elements';
 import * as shortid from 'shortid';
 
 import SKILL_TREE_DATA from './SKILL_TREE_DATA';
-import AppFonts from '../../theme/fonts'
+import AppFonts from '../../theme/fonts';
 
 // eslint-disable-next-line import/no-unresolved
 const filter = require('lodash.filter');
@@ -21,14 +21,31 @@ class SkillDetailView extends Component {
   };
 
   render() {
-    const skillData = filter(SKILL_TREE_DATA, { id: this.props.skillId })[0];
+    const skillData = filter(SKILL_TREE_DATA, {id: this.props.skillId})[0];
     let skillLinkList = null;
     if (skillData.links) {
       skillLinkList = (<View>
-        <Text style={{ paddingTop: 15, paddingLeft: 15, fontSize: AppFonts.h4.size }}>推荐资料</Text>
-        <List containerStyle={{ marginBottom: 20 }}>
+        <Text style={{ paddingTop: 15, paddingLeft: 15, fontSize: AppFonts.h4.size }}>扩展资料</Text>
+        <List>
           {
             skillData.links.map(link => (
+              <ListItem
+                key={shortid.generate()}
+                title={link.label}
+              />
+            ))
+          }
+        </List>
+      </View>);
+    }
+
+    let skillBookList = null;
+    if (skillData.links) {
+      skillBookList = (<View>
+        <Text style={{ paddingTop: 15, paddingLeft: 15, fontSize: AppFonts.h4.size }}>推荐书籍</Text>
+        <List containerStyle={{ marginBottom: 20 }}>
+          {
+            skillData.books.map(link => (
               <ListItem
                 key={shortid.generate()}
                 title={link.label}
@@ -47,6 +64,7 @@ class SkillDetailView extends Component {
           <Text>{ skillData.description }</Text>
         </Card>
         {skillLinkList}
+        {skillBookList}
       </View>
     );
   }
