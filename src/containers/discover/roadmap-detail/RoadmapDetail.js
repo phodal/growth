abe0e785314396b79f188761c5faa85b74a5fbf1
travelgeezer <travelgeezer@icloud.com>
2017-05-13 22:07:37 +0800

    [Discover] roadmap-detail: create roadmap detail & add time line view

---
+++ src/containers/discover/roadmap-detail/RoadmapDetail.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import { ScrollView } from 'react-native';
+import TimeLine from '../TimeLineView';
+import AppSize from '../../../theme/sizes';
+
+const parse = (value, re) => (value.replace(re, '\r\n'));
+
+class RoadmapDetail extends Component {
+  static componentName = 'RoadmapDetail';
+
+  static propTypes = {
+    timeline: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string,
+        section: PropTypes.string,
+      }),
+    ),
+  };
+
+  static defaultProps = {
+    timeline: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rowData: Array.from(
+        new Array(this.props.timeline.length))
+        .map((val, index) => (
+          this.props.timeline[index]
+        )),
+    };
+  }
+
+  render() {
+    const rows = this.state.rowData.map((val, index) => (
+      <TimeLine
+        title={val.section}
+        content={parse(parse(val.description, /<br\s*\/?> /gi), /<br\s*\/?>/gi)}
+        itemIndex={index}
+        key={val.section}
+      />
+    ));
+    return (
+      <ScrollView style={{ marginTop: AppSize.navbarHeight }}>
+        {rows}
+      </ScrollView>
+    );
+  }
+}
+export default RoadmapDetail;
