b011471bf798ae4c8e832035181d60a4a678c2fc
travelgeezer <travelgeezer@icloud.com>
2017-05-13 22:06:26 +0800

    [Discover] roadmap-list: update content & add onclick event

--- src/containers/discover/roadmap-list/RoadmapList.js
+++ src/containers/discover/roadmap-list/RoadmapList.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ScrollView, View, Text, TouchableHighlight } from 'react-native';
+import Api from '../../../utils/api';
+import AppSize from '../../../theme/sizes';
+import Launch from '../Launch';
+
+async function load(call) {
+  Api.getRoadMapListData()
+    .then(response => (call(response.data)))
+    .catch(error => (console.log(error)));
+}
 
 class RoadmapList extends Component {
   static componentName = 'RoadmapList';
-  render = () => (
-    <Text>RoadmapList</Text>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rowData: [],
+    };
+    load((json => (
+      this.setState(
+        this.state.rowData = (
+          Array.from(new Array(json.content.length)).map(
+            (val, index) => (
+              json.content[index])))))));
+  }
+  render() {
+    const rows = this.state.rowData.map(data => (
+      <TouchableHighlight
+        onPress={() => { Launch.roadmapDetail(data.name.concat('学习路线'), data.timeline); }}
+        underlayColor="rgba(250, 250, 250, .9)"
+        key={data.name}
+        style={{ backgroundColor: 'white' }}
+      >
+        <View style={{ marginLeft: 15, marginRight: 15 }}>
+          <Text style={{ flex: 1, height: 40, lineHeight: 40 }}>{data.name}</Text>
+          <Text style={{ flex: 1, height: 1, backgroundColor: 'rgba(240, 240, 240, .9)' }} />
+        </View>
+      </TouchableHighlight>
+    ));
+    return (
+      <ScrollView style={{ marginTop: AppSize.navbarHeight }} >
+        {rows}
+      </ScrollView>
+    );
+  }
 }
 export default RoadmapList;
+
