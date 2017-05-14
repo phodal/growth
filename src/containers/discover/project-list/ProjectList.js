7d6798056cc03ce19ce948ce3f74343c9998e7b0
travelgeezer <travelgeezer@icloud.com>
2017-05-14 17:02:02 +0800

    [Discover] project: update project list view & add jump detail event

--- src/containers/discover/project-list/ProjectList.js
+++ src/containers/discover/project-list/ProjectList.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
+import Api from '../../../utils/api';
+import AppSize from '../../../theme/sizes';
+import GroupList from './ProjectListItem';
+
+async function load(call) {
+  Api.getProjectListData()
+    .then(response => (call(response.data)))
+    .catch(error => (error));
+}
 
 class ProjectList extends Component {
   static componentName = 'ProjectList';
-  render = () => (
-    <Text>ProjectList</Text>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rowData: [],
+    };
+    load((json => (
+      this.setState(
+        this.state.rowData = (
+          Array.from(new Array(json.content.length)).map(
+            (val, index) => (
+              json.content[index])))))));
+  }
+
+  render() {
+    const rows = this.state.rowData.map((val, index) => (
+      <GroupList key={val.name} content={this.state.rowData[index]} />));
+    return (
+      <ScrollView style={{ marginTop: AppSize.navbarHeight }}>
+        {rows}
+      </ScrollView>);
+  }
+
 }
 export default ProjectList;
