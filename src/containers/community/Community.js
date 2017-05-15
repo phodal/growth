5715c1ffbe94d1181ecdffe4aa760372dda0f58e
Fengda HUANG <h@phodal.com>
2017-05-16 07:40:01 +0800

    [Community] create a simple view

--- src/containers/community/Community.js
+++ src/containers/community/Community.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { PureComponent } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 
-class Community extends Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const FirstRoute = () => <View style={[styles.container, { backgroundColor: '#ff4081' }]} />;
+const SecondRoute = () => <View style={[styles.container, { backgroundColor: '#673ab7' }]} />;
+
+class Community extends PureComponent {
   static componentName = 'Community';
 
-  render = () => (
-    <View>
-      <Text>Community</Text>
-    </View>
-  );
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: '论坛' },
+      { key: '2', title: '动态' },
+    ],
+  };
+
+  handleChangeTab = index => this.setState({ index });
+
+  renderHeader = props => <TabBar {...props} />;
+
+  renderScene = SceneMap({
+    1: FirstRoute,
+    2: SecondRoute,
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this.renderScene}
+        renderHeader={this.renderHeader}
+        onRequestChangeTab={this.handleChangeTab}
+      />
+    );
+  }
 }
 
 export default Community;
