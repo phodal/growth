22954a3967263f84586e172b244d1cbac927463e
Fengda HUANG <h@phodal.com>
2017-05-16 20:52:44 +0800

    [Test] create test for helper

--- __tests__/navigation/index.js
+++ __tests__/navigation/index.js
@@ -1,20 +1,18 @@
 import 'react-native';
-import { expect } from 'chai';
-
 import scenes from '../../src/navigation/index';
 
 describe('router test', () => {
   it('test router should currently', () => {
-    expect(scenes.comingSoon.component.name).to.equal('ComingSoonView');
+    expect(scenes.comingSoon.component.name).toBe('ComingSoonView');
   });
 
   it('should return correct tabs key', () => {
     const tabs = scenes.tabs.children;
 
-    expect(tabs[0]).to.eq('home');
-    expect(tabs[1]).to.eq('discover');
-    expect(tabs[2]).to.eq('skillTree');
-    expect(tabs[3]).to.eq('community');
-    expect(tabs[4]).to.eq('userCenter');
+    expect(tabs[0]).toBe('home');
+    expect(tabs[1]).toBe('discover');
+    expect(tabs[2]).toBe('skillTree');
+    expect(tabs[3]).toBe('community');
+    expect(tabs[4]).toBe('userCenter');
   });
 });
