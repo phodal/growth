fadbfb55acf013d266ce73981104f2bbb95187cc
travelgeezer <travelgeezer@icloud.com>
2017-05-17 00:12:27 +0800

    [Discover] toolbox: update toolbox list component

--- src/containers/discover/toolbox-list/ToolBoxList.js
+++ src/containers/discover/toolbox-list/ToolBoxList.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import AppStyle from '../../../theme/styles';
+import Api from '../../../utils/api';
+import Dialog from '../../../utils/dialog';
+import SimpleListItem from '../SimpleListItem';
+import Launch from '../Launch';
+
+async function load(call) {
+  Api.getToolBoxData()
+    .then(response => (call(response.data)))
+    .catch(error => error);
+}
 
 class ToolBoxList extends Component {
   static componentName = 'ToolBoxList';
-  render = () => (
-    <Text>ToolBoxList</Text>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      rowData: [],
+    };
+  }
+
+  componentDidMount() {
+    load(json => (
+      this.setState({
+        loading: false,
+        rowData: Array.from(new Array(json.content.length))
+        .map((val, index) => json.content[index]) })
+     ));
+  }
+
+  render() {
+    const rows = this.state.rowData.map(val => (
+      <SimpleListItem
+        key={val.title}
+        text={val.title}
+        click={() => { Launch.toolBoxDetail(val.path); }}
+      />
+    ));
+    return (<ScrollView style={AppStyle.detailBasisStyle}>
+      <Dialog show={this.state.loading} />
+      {
+        !this.state.loading ?
+        rows : null
+      }
+    </ScrollView>);
+  }
 }
 export default ToolBoxList;
