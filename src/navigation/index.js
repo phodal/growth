6b5d7c638069a757ff9a1a01e189178b0b6c23f9
Fengda HUANG <h@phodal.com>
2017-05-16 20:19:01 +0800

    [T] add open link for skill tree

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -27,6 +27,7 @@ import ChapterList from '../containers/discover/chapter-list/ChapterList';
 import SkillTree from '../containers/skill-tree/SkillTree';
 import SkillDetailView from '../containers/skill-tree/SkillDetailView';
 import ComingSoonView from '../containers/ComingSoonView';
+import Helper from '../utils/helper';
 
 
 const navbarPropsTabs = {
@@ -87,7 +88,7 @@ export default Actions.create(
         key={'skillTree'}
         title={'技能树'}
         rightTitle={'获取专业版'}
-        onRight={() => Actions.comingSoon()}
+        onRight={() => Helper.getProfessionalSkilltree()}
         rightButtonTextStyle={AppStyles.navbarTitle}
         iconName={'md-egg'}
         iconType={'ionicon'}
