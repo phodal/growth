4a452e0299167dfd4b292b3e16429a7da4a5363d
Fengda HUANG <h@phodal.com>
2017-05-19 07:39:02 +0800

    [Home] create basic layout

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Card from 'react-native-elements/src/containers/Card';
 
 class Home extends Component {
   static componentName = 'Home';
 
   render = () => (
     <View>
-      <Text>Hello</Text>
+      <Card
+        containerStyle={{ borderRadius: 5 }}
+      >
+        <Text style={{ fontSize: 20, marginTop: 5, marginBottom: 20 }}>从零开始</Text>
+        <Text style={{ marginTop: 5, marginBottom: 15 }}>
+          从这里准备开始你的技术图谱修炼之路
+        </Text>
+      </Card>
     </View>
   );
 }
