595b0118f2f9a041f3c7e1773c7833d99ab09f57
Phodal Huang <h@phodal.com>
2017-05-18 15:00:35 +0800

    [UserCenter] test: add share friend

--- __tests__/containers/user-center/userCenter.test.js
+++ __tests__/containers/user-center/userCenter.test.js
@@ -1,6 +1,7 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Share from 'react-native-share';
 
 import UserCenter from '../../../src/containers/user-center/UserCenter';
 
@@ -12,5 +13,15 @@ describe('test user center', () => {
 
     const treeJson = tree.toJSON();
     expect(treeJson).toMatchSnapshot();
+  });
+
+  it('should call share to friend function when click button', () => {
+    const spy = jest.spyOn(Share, 'open');
+    UserCenter.shareToFriend();
+    expect(spy).toBeCalledWith({
+      title: 'Growth：带你成为顶尖开发者',
+      message: '我现在使用Growth，这是一款专注于Web开发者成长的应用，涵盖Web开发的流程及技术栈，Web开发的学习路线、成长衡量等各方面。快来下载吧!',
+      url: 'http://growth.ren/',
+    });
   });
 });
