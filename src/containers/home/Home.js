d3ca919df29c4a22f5b870403652fd6c51b02e8f
Phodal Huang <h@phodal.com>
2017-05-19 14:28:09 +0800

    [Home] create shadow

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
 import AppColors from '../../theme/colors';
+
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+    margin: 10,
+    height: 100,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(0,0,0, .2)',
+        shadowOffset: { height: 1, width: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 1,
+      },
+      android: {
+        elevation: 1,
+      },
+    }),
+  },
+});
 
 class Home extends Component {
   static componentName = 'Home';
 
   render = () => (
-    <View style={{ backgroundColor: '#fff', borderTopLeftRadius: 6, borderTopRightRadius: 6, margin: 10, height: 100 }}>
-      <View style={{ borderRadius: 5, flex: 1, flexDirection: 'row' }}>
+    <View style={styles.cardContainer}>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
         <View style={{ flex: 1, justifyContent: 'space-between', flexDirection: 'column', margin: 15 }}>
           <View style={{ height: 40 }}>
             <Text style={{ fontSize: 20, marginTop: 5, marginBottom: 15 }}>从零开始</Text>
