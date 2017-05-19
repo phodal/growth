e03dc02ff674e3aa758db6058d500ac34c22ebef
Phodal Huang <h@phodal.com>
2017-05-19 17:43:07 +0800

    [Home] try to create collapsible super card view

--- src/components/SuperCardView.js
+++ src/components/SuperCardView.js
@@ -1,6 +1,12 @@
 import React, { PropTypes } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import {
+  View, Text, StyleSheet, Platform,
+  TouchableHighlight,
+} from 'react-native';
 import { Icon } from 'react-native-elements';
+import Accordion from 'react-native-collapsible/Accordion';
+import * as Animatable from 'react-native-animatable';
+
 import AppColors from '../theme/colors';
 
 const styles = StyleSheet.create({
@@ -24,30 +30,89 @@ const styles = StyleSheet.create({
   },
 });
 
-class SuperCardView extends React.Component {
+const SECTIONS = [
+  {
+    title: 'First',
+    content: 'Lorem ipsum...',
+  },
+  {
+    title: 'Second',
+    content: 'Lorem ipsum...',
+  },
+];
+
+class SuperCardView extends React.PureComponent {
+
+  static renderContent(section, i, isActive) {
+    return (
+      <Animatable.View
+        duration={300}
+        transition="backgroundColor"
+        style={{ backgroundColor: (isActive ? 'rgba(255,255,255,1)' : 'rgba(245,252,255,1)') }}
+      >
+        <Animatable.Text
+          duration={300}
+          easing="ease-out"
+          animation={isActive ? 'zoomIn' : false}
+        >
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  static renderHeader(section, index, isActive) {
+    return (
+      <Animatable.View
+        duration={300}
+        transition="backgroundColor"
+      >
+        <View style={styles.cardContainer}>
+          <TouchableHighlight
+            underlayColor="#fff"
+            onPress={() => {}}
+          >
+            <View>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={{ flex: 1, justifyContent: 'space-between', flexDirection: 'column', margin: 15 }}>
+                  <View style={{ height: 40 }}>
+                    <Text style={{ fontSize: 20, marginTop: 5, marginBottom: 15 }}>
+                      {this.props.title}
+                    </Text>
+                  </View>
+                  <View style={{ height: 40 }}>
+                    <Text style={{ marginTop: 5, marginBottom: 15 }}>
+                      {this.props.subTitle}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <View style={{ height: 100, alignItems: 'flex-end', left: -10 }}>
+                <Icon size={100} name={this.props.iconName} type={'ionicon'} color={AppColors.brand.primary} />
+              </View>
+            </View>
+          </TouchableHighlight>
+        </View>
+      </Animatable.View>
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.renderContent = SuperCardView.renderContent.bind(this);
+    this.renderHeader = SuperCardView.renderHeader.bind(this);
+  }
+
   componentName = 'SuperCardView';
+
 
   render() {
     return (
-      <View style={styles.cardContainer}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1, justifyContent: 'space-between', flexDirection: 'column', margin: 15 }}>
-            <View style={{ height: 40 }}>
-              <Text style={{ fontSize: 20, marginTop: 5, marginBottom: 15 }}>
-                {this.props.title}
-              </Text>
-            </View>
-            <View style={{ height: 40 }}>
-              <Text style={{ marginTop: 5, marginBottom: 15 }}>
-                {this.props.subTitle}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ height: 100, alignItems: 'flex-end', left: -10 }}>
-          <Icon size={100} name={this.props.iconName} type={'ionicon'} color={AppColors.brand.primary} />
-        </View>
-      </View>
+      <Accordion
+        sections={SECTIONS}
+        renderHeader={this.renderHeader}
+        renderContent={this.renderContent}
+      />
     );
   }
 }
