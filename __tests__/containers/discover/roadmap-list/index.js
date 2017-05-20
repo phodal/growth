46a46e145fa8f3b947c3ea9f53a445dee97d5401
travelgeezer <travelgeezer@icloud.com>
2017-05-20 16:25:52 +0800

    [T] test: test roadmap list

---
+++ __tests__/containers/discover/roadmap-list/index.js
@@ -0,0 +1,13 @@
+import 'react-native';
+import React, {} from 'react';
+import renderer from 'react-test-renderer';
+import RoadmapList from '../../../../src/containers/discover/roadmap-list/RoadmapList';
+import TIPS from '../../../../assets/TIPS';
+
+it('renders correctly', () => {
+  const roadmapList = renderer.create(
+    <RoadmapList dialogContent={TIPS[0]} />,
+  );
+  const roadmapListJson = roadmapList.toJSON();
+  expect(roadmapListJson).toMatchSnapshot();
+});
