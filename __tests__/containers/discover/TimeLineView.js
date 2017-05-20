4a56d35f8484aa048d9f58a36de0e812ef5fb2a0
travelgeezer <travelgeezer@icloud.com>
2017-05-20 17:29:37 +0800

    [T] test: test discover time line view

---
+++ __tests__/containers/discover/TimeLineView.js
@@ -0,0 +1,18 @@
+import 'react-native';
+import React, {} from 'react';
+import renderer from 'react-test-renderer';
+import TimeLineView from '../../../src/containers/discover/TimeLineView';
+
+
+it('renders correctly', () => {
+  const timeLine = renderer.create(
+    <TimeLineView
+      title={'title'}
+      content={'23333333'}
+      itemIndex={0}
+      key={'key'}
+    />,
+  );
+  const timeLineJson = timeLine.toJSON();
+  expect(timeLineJson).toMatchSnapshot();
+});
