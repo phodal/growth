9bac22cad3c4cade3ce7339cb0da6443764512e3
travelgeezer <travelgeezer@icloud.com>
2017-05-20 17:29:22 +0800

    [T] test: test discover dimple list itme

---
+++ __tests__/containers/discover/SimpleListItem.js
@@ -0,0 +1,17 @@
+import 'react-native';
+import React, {} from 'react';
+import renderer from 'react-test-renderer';
+import SimpleListItem from '../../../src/containers/discover/SimpleListItem';
+
+
+it('renders correctly', () => {
+  const simplelist = renderer.create(
+    <SimpleListItem
+      key={'233333'}
+      text={'23333'}
+      click={() => {}}
+    />,
+  );
+  const simplelistJson = simplelist.toJSON();
+  expect(simplelistJson).toMatchSnapshot();
+});
