4b8f39aa3fc7fcc6db3d1be773c40d636adb299f
airt <airtial@gmail.com>
2017-05-21 22:56:34 +0800

    [UserCenter] about-us: update about-us view

--- src/containers/user-center/AboutUs.js
+++ src/containers/user-center/AboutUs.js
@@ -1,8 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Linking, ScrollView, Text, View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import AppStyle from '../../theme/styles';
+
+const contributors = [{
+  avatar: 'https://facebook.github.io/react/img/logo_og.png',
+  name: 'Name Placeholder',
+  description: 'Placeholder',
+}, {
+  avatar: 'https://facebook.github.io/react/img/logo_og.png',
+  name: 'Name Placeholder 1',
+  description: 'Placeholder 1',
+}];
+
+const Label = props => (
+  <View style={{ paddingLeft: 20, marginTop: 12, marginBottom: -6 }}>
+    <Text style={{ fontSize: 14 }}>
+      {props.text}
+    </Text>
+  </View>
+);
+
+Label.propTypes = {
+  text: React.PropTypes.string.isRequired,
+};
 
 const AboutUs = () => (
-  <Text>AboutUs</Text>
+  <ScrollView style={AppStyle.userCenterBasisStyle}>
+    <Label text="Growth" />
+    <List>
+      <ListItem
+        title={'开源 - GitHub'}
+        rightIcon={{ name: 'github', type: 'font-awesome' }}
+        onPress={() => Linking.openURL('https://github.com/phodal/growth-ng')}
+      />
+      <ListItem
+        title={'QQ 交流群: 529600495'}
+        hideChevron
+      />
+      <ListItem
+        title={'论坛: https://forum.growth.ren'}
+        onPress={() => Linking.openURL('https://forum.growth.ren')}
+        hideChevron
+      />
+    </List>
+    <Label text="贡献者" />
+    <List>
+      {contributors.map(contributor =>
+        <ListItem
+          key={contributor.name}
+          title={contributor.name}
+          subtitle={contributor.description}
+          avatar={{ uri: contributor.avatar }}
+          roundAvatar
+          hideChevron
+        />,
+      )}
+    </List>
+  </ScrollView>
 );
 
 export default AboutUs;
