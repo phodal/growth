b6d46fc7f7fe922adce5f357414bb3c35750afc3
travelgeezer <travelgeezer@icloud.com>
2017-05-23 16:26:05 +0800

    [Discover] exam: create exam detail

---
+++ src/containers/discover/exam-detail/ExamDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import AppStyle from '../../../theme/styles';
+
+class ExamDetail extends Component {
+  static componentName = 'ExamDetail';
+
+  render() {
+    return <Text style={AppStyle.detailBasisStyle}>2333</Text>;
+  }
+}
+export default ExamDetail;
