82be485737c408ec8667e3840fbc115faf5e21be
travelgeezer <travelgeezer@icloud.com>
2017-05-23 16:07:16 +0800

    [T] data: add exam static resource

---
+++ assets/data/QUIZS.js
@@ -0,0 +1,16 @@
+import FE from './quiz/FE';
+import CLEAN_CODE from './quiz/CLEAN_CODE';
+import HTML from './quiz/HTML';
+import CSS from './quiz/CSS';
+import JAVASCRIPT from './quiz/JAVASCRIPT';
+import NODEJS from './quiz/NODEJS';
+
+const QUIZS = {
+  fe: FE,
+  clean_code: CLEAN_CODE,
+  css: CSS,
+  html: HTML,
+  javascript: JAVASCRIPT,
+  nodejs: NODEJS,
+};
+export default QUIZS;
