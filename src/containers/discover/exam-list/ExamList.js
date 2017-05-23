52721a00ab0f09e0064296502265a1ad9b5f3027
travelgeezer <travelgeezer@icloud.com>
2017-05-23 16:19:56 +0800

    [Discover] exam: update exam list view

--- src/containers/discover/exam-list/ExamList.js
+++ src/containers/discover/exam-list/ExamList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import AppStyle from '../../../theme/styles';
+import SimpleListItem from '../../../components/discover/view/SimpleListItem';
+import EXAM_LIST from '../../../../assets/data/EXAM_LIST';
 
 class ExamList extends Component {
   static componentName = 'ExamList';
-  render = () => (
-    <ScrollView style={AppStyle.detailBasisStyle}>ExamList</ScrollView>
-  );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rowData: EXAM_LIST,
+    };
+  }
+
+  render() {
+    const rows = this.state.rowData
+        .map((val, index) => (
+          <SimpleListItem text={val} key={val.concat(index)} />));
+
+    return (
+      <ScrollView style={AppStyle.detailBasisStyle}>
+        {rows}
+      </ScrollView>);
+  }
 }
 export default ExamList;
