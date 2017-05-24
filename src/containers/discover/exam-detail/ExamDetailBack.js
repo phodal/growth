dd199b36c537a3efec7a116bb3a442379d4d0a2f
travelgeezer <travelgeezer@icloud.com>
2017-05-24 17:47:36 +0800

    [Discover] exam: create exam detail back view

---
+++ src/containers/discover/exam-detail/ExamDetailBack.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, TouchableHighlight, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import AppStyle from '../../../theme/styles';
+
+class ExamDetailBack extends Component {
+  static componentName = 'ExamDetailBack';
+  render() {
+    return (
+      <View style={{ height: 40, flexDirection: 'row' }}>
+        <View style={{ flex: 1 }} />
+        <TouchableHighlight
+          onPress={() => Actions.pop()}
+          style={AppStyle.examDetailBackStyle}
+        >
+          <Text style={{ color: '#03a9f4' }}>bakc</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+export default ExamDetailBack;
