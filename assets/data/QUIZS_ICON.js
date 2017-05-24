c827fe9746e016a39359b9afe47fa73dc8d01c36
travelgeezer <travelgeezer@icloud.com>
2017-05-24 17:44:19 +0800

    [T] assets: add quizs use icon

---
+++ assets/data/QUIZS_ICON.js
@@ -0,0 +1,16 @@
+const ICON_LIST = [
+  'md-ionic',
+  'md-aperture',
+  'md-at',
+  'md-baseball',
+  'md-basket',
+  'md-beer',
+  'md-bicycle',
+  'md-boat',
+  'md-bonfire',
+  'md-bowtie',
+];
+export default ICON_LIST;
+export const iconSize = 15;
+export const iconType = 'ionicon';
+export const defaultIcon = 'md-egg';
