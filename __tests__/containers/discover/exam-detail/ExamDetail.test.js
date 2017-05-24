ce7d0f5e3ac766305ccdb1a8b5fd6719354acef3
travelgeezer <travelgeezer@icloud.com>
2017-05-24 18:05:35 +0800

    [T] test: test exam detail

---
+++ __tests__/containers/discover/exam-detail/ExamDetail.test.js
@@ -0,0 +1,13 @@
+import 'react-native';
+import React, {} from 'react';
+import renderer from 'react-test-renderer';
+import ExamDetail from '../../../../src/containers/discover/exam-detail/ExamDetail';
+import QUIZS from '../../../../assets/data/QUIZS';
+
+it('renders correctly', () => {
+  const examDetail = renderer.create(
+    <ExamDetail quizs={QUIZS.fe} test />,
+  );
+  const examDetailJson = examDetail.toJSON();
+  expect(examDetailJson).toMatchSnapshot();
+});
