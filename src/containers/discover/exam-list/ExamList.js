df7d72d8eeaf7ce78c2e358c7cfcf683785f6c15
travelgeezer <travelgeezer@icloud.com>
2017-05-24 17:45:14 +0800

    [Discover] exam: update exam list

--- src/containers/discover/exam-list/ExamList.js
+++ src/containers/discover/exam-list/ExamList.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import AppStyle from '../../../theme/styles';
 import SimpleListItem from '../../../components/discover/view/SimpleListItem';
+import Launch from '../../../components/discover/Launch';
 import EXAM_LIST from '../../../../assets/data/EXAM_LIST';
+import QUIZS from '../../../../assets/data/QUIZS';
+
+function swtichQuiz(name) {
+  switch (name) {
+    case 'HTML':
+      return QUIZS.html;
+    case 'CSS':
+      return QUIZS.css;
+    case 'JavaScript':
+      return QUIZS.javascript;
+    case '前端':
+      return QUIZS.fe;
+    case 'Node.js':
+      return QUIZS.nodejs;
+    default:
+      return [];
+  }
+}
 
 class ExamList extends Component {
   static componentName = 'ExamList';
@@ -17,7 +36,11 @@ class ExamList extends Component {
   render() {
     const rows = this.state.rowData
         .map((val, index) => (
-          <SimpleListItem text={val} key={val.concat(index)} />));
+          <SimpleListItem
+            text={val}
+            click={() => (Launch.examDetail(swtichQuiz(val)))}
+            key={val.concat(index)}
+          />));
 
     return (
       <ScrollView style={AppStyle.detailBasisStyle}>
