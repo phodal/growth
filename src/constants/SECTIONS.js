e03248616824842cdf57afc3e69eeec2e124f2ed
Phodal Huang <h@phodal.com>
2017-05-26 16:48:12 +0800

    [T] update sections field

--- src/constants/SECTIONS.js
+++ src/constants/SECTIONS.js
@@ -1,39 +1,37 @@
-import { SECTION0 } from './sections/section-0';
-import { SECTION1 } from './sections/section-1';
-import { SECTION2 } from './sections/section-2';
-import { SECTION3 } from './sections/section-3';
-import { SECTION4 } from './sections/section-4';
-import { SECTION5 } from './sections/section-5';
-import { SECTION6 } from './sections/section-6';
-import { SECTION7 } from './sections/section-7';
+import SECTION0 from './sections/section-0';
+import SECTION1 from './sections/section-1';
+import SECTION2 from './sections/section-2';
+import SECTION3 from './sections/section-3';
+import SECTION4 from './sections/section-4';
+import SECTION5 from './sections/section-5';
+import SECTION6 from './sections/section-6';
+import SECTION7 from './sections/section-7';
 
-const SECTIONS = {};
-
-SECTIONS.hello = SECTION0.hello;
-SECTIONS.env = SECTION0.env;
-
-SECTIONS.build = SECTION1.build;
-SECTIONS.front = SECTION1.front;
-SECTIONS.backend = SECTION1.backend;
-
-SECTIONS.before = SECTION2.before;
-SECTIONS.test = SECTION2.test;
-SECTIONS.coding = SECTION2.coding;
-
-SECTIONS.container = SECTION3.container;
-SECTIONS.server = SECTION3.server;
-SECTIONS.configurable = SECTION3.configurable;
-
-SECTIONS.analytics = SECTION4.analytics;
-SECTIONS.ux = SECTION4.ux;
-
-SECTIONS.ci = SECTION5.ci;
-SECTIONS.cd = SECTION5.cd;
-
-SECTIONS.legacy = SECTION6.legacy;
-SECTIONS.refactor = SECTION6.refactor;
-
-SECTIONS.retro = SECTION7.retro;
-SECTIONS.arch = SECTION7.arch;
+const SECTIONS = [
+  [
+    SECTION0.hello, SECTION0.env,
+  ],
+  [
+    SECTION1.build, SECTION1.front, SECTION1.backend,
+  ],
+  [
+    SECTION2.before, SECTION2.test, SECTION2.coding,
+  ],
+  [
+    SECTION3.container, SECTION3.server, SECTION3.configurable,
+  ],
+  [
+    SECTION4.analytics, SECTION4.ux,
+  ],
+  [
+    SECTION5.ci, SECTION5.cd,
+  ],
+  [
+    SECTION6.legacy, SECTION6.refactor,
+  ],
+  [
+    SECTION7.retro, SECTION7.arch,
+  ],
+];
 
 export default SECTIONS;
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import SuperCardView from '../../components/SuperCardView';
+import SECTIONS from '../../constants/SECTIONS';
 
 class Home extends Component {
   static componentName = 'Home';
 
-  render = () => (
-    <View>
-      <SuperCardView title={'从零开始'} subTitle={'从这里准备开始你的技术图谱修炼之路'} iconName={'md-home'} />
-    </View>
-  );
+  render() {
+    console.log(SECTIONS);
+
+    return (
+      <View>
+        <SuperCardView title={'从零开始'} subTitle={'从这里准备开始你的技术图谱修炼之路'} iconName={'md-home'} />
+      </View>
+    );
+  }
 }
 
 export default Home;
