ba82f6dec4f8a012bab1b44dfc986443ffe900a8
travelgeezer <travelgeezer@icloud.com>
2017-05-27 21:38:49 +0800

    [Discover] thoughtworks: create domain detail page compontent

---
+++ src/containers/discover/thoughtworks-books/domain-detail-page/DomainDetailPage.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react';
+import { View } from 'react-native';
+import AppStyle from '../../../../theme/styles';
+import TWBOOKS from '../../../../constants/TWBOOKS';
+import PageItem from '../../../../components/discover/thought-works-books/view/DomainDetailPageItem';
+
+function getBooks(domain) {
+  switch (domain) {
+    case 'practical':
+      return TWBOOKS['zh-cn'].practical.books;
+    case 'architecture':
+      return TWBOOKS['zh-cn'].architecture.books;
+    case 'methodology':
+      return TWBOOKS['zh-cn'].methodology.books;
+    case 'leadership':
+      return TWBOOKS['zh-cn'].leadership.books;
+    default:
+      return [];
+  }
+}
+
+class DomainDetailPage extends Component {
+  static componentName = 'DomainDetailPage';
+
+  static propTypes = {
+    domain: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    domain: 'practical',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: getBooks(this.props.domain),
+      data: [
+        { name: '基础篇', books: getBooks(this.props.domain).basic },
+        { name: '进阶篇', books: getBooks(this.props.domain).intermediate },
+        { name: '高级篇', books: getBooks(this.props.domain).advanced },
+      ],
+    };
+  }
+
+  render() {
+    const rows = this.state.data.map((val, index) => <PageItem content={val} key={'key'.concat(index)} />);
+    return <View style={AppStyle.detailBasisStyle}>{rows}</View>;
+  }
+}
+export default DomainDetailPage;
