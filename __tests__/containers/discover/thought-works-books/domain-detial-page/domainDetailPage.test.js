7106097c18b18077c93f2c028aac3d42d9ad9f78
travelgeezer <travelgeezer@icloud.com>
2017-05-27 21:56:26 +0800

    [T] test: add test domain detail page compontent

---
+++ __tests__/containers/discover/thought-works-books/domain-detial-page/domainDetailPage.test.js
@@ -0,0 +1,12 @@
+import 'react-native';
+import React, {} from 'react';
+import renderer from 'react-test-renderer';
+import DomainDetailPage from '../../../../../src/containers/discover/thoughtworks-books/domain-detail-page/DomainDetailPage';
+
+it('renders correctly', () => {
+  const domainDetailPage = renderer.create(
+    <DomainDetailPage />,
+  );
+  const domainDetailPageJson = domainDetailPage.toJSON();
+  expect(domainDetailPageJson).toMatchSnapshot();
+});
