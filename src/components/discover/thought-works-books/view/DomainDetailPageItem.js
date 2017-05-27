f1e9e55e9918b89d6c105cd9e29b7b1da5da1b4d
travelgeezer <travelgeezer@icloud.com>
2017-05-27 21:36:37 +0800

    [Discover] thoughtworkbooks: create domain detail page item compontent

---
+++ src/components/discover/thought-works-books/view/DomainDetailPageItem.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import { Text, View } from 'react-native';
+import AppStyle from '../../../../theme/styles';
+import Line from '../../../Line';
+
+class DomainDetailPageItem extends Component {
+  static componentName = 'DomainDetailPageItem';
+
+  static propTypes = {
+    content:
+        PropTypes.shape(
+          PropTypes.string.isRequired,
+          PropTypes.arrayOf(PropTypes.shape(
+            PropTypes.string.isRequired,
+            ),
+          )),
+  };
+
+  static defaultProps = {
+    content: {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.content.name,
+      rowData: Array.from(new Array(this.props.content.books.length))
+        .map((val, index) => (this.props.content.books[index])) };
+  }
+
+  render() {
+    const rows = this.state.rowData.map((val, index) => (
+      <View style={{ backgroundColor: 'white' }} key={val.concat(index)}>
+        <View style={AppStyle.projectListContentStyle}>
+          <Text>{val}</Text>
+        </View>
+        <Line />
+      </View>
+     ));
+    return (
+      <View>
+        <View style={AppStyle.projectListTitleStyle}>
+          <Text style={{ color: '#999' }}>{this.state.name}</Text>
+        </View>
+        {rows}
+      </View>
+    );
+  }
+}
+export default DomainDetailPageItem;
+
