94d10664289580a82778ffbab58321cc47a76edd
Phodal HUANG <h@phodal.com>
2017-05-28 21:32:59 +0800

    [ForumView] test: try to test props

--- __tests__/containers/community/forum/forumView.test.js
+++ __tests__/containers/community/forum/forumView.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import * as chai from 'chai';
+import { shallow } from 'enzyme';
+import { ListView } from 'react-native';
+
 import ForumView from '../../../../src/containers/community/forum/ForumView';
 
-describe('test user center', () => {
+const mockData = require('./mockdata.json');
+
+describe('test forum render', () => {
   it('renders correctly', () => {
     const tree = renderer.create(
       <ForumView
@@ -17,5 +23,26 @@ describe('test user center', () => {
 
     const treeJson = tree.toJSON();
     expect(treeJson).toMatchSnapshot();
+  });
+});
+
+describe('test forum view props', () => {
+  it('should refetch data success', () => {
+    const mockFetch = jest.fn();
+
+    const wrapper = shallow(<ForumView
+      data={mockData.data}
+      canLoadMoreContent
+      onLoadMoreAsync={() => {}}
+      reFetch={mockFetch}
+      dataUrl={'https://forum.growth.ren/api/discussions'}
+      element={() => {}}
+    />);
+
+    expect(wrapper).toBeDefined();
+    chai.expect(wrapper.find(ListView)).to.have.length(1);
+
+    // wrapper.find(ListView).props.refreshControl;
+    // expect(mockFetch).toHaveBeenCalled();
   });
 });
