f8a99dfc1ca83ea4dcd8f892cfa8b85961bb6107
travelgeezer <travelgeezer@icloud.com>
2017-05-28 12:05:04 +0800

    [T] constants: add BOOKS file && RECOMMEND_BOOKS_LIST file

---
+++ src/constants/RECOMMEND_BOOKS_LIST.js
@@ -0,0 +1,20 @@
+import DOMAIN_NAME from './DOMAIN_NAME';
+import BOOKS from './BOOKS';
+
+const RECOMMEND_BOOKS_LIST = [
+  { name: DOMAIN_NAME.analytics, books: BOOKS['zh-cn'].analytics },
+  { name: DOMAIN_NAME.arch, books: BOOKS['zh-cn'].arch },
+  { name: DOMAIN_NAME.backend, books: BOOKS['zh-cn'].backend },
+  { name: DOMAIN_NAME.before, books: BOOKS['zh-cn'].before },
+  { name: DOMAIN_NAME.build, books: BOOKS['zh-cn'].build },
+  { name: DOMAIN_NAME.cd, books: BOOKS['zh-cn'].cd },
+  { name: DOMAIN_NAME.coding, books: BOOKS['zh-cn'].coding },
+  { name: DOMAIN_NAME.configurable, books: BOOKS['zh-cn'].configurable },
+  { name: DOMAIN_NAME.front, books: BOOKS['zh-cn'].front },
+  { name: DOMAIN_NAME.hello, books: BOOKS['zh-cn'].hello },
+  { name: DOMAIN_NAME.legacy, books: BOOKS['zh-cn'].legacy },
+  { name: DOMAIN_NAME.refactor, books: BOOKS['zh-cn'].refactory },
+  { name: DOMAIN_NAME.server, books: BOOKS['zh-cn'].server },
+  { name: DOMAIN_NAME.ux, books: BOOKS['zh-cn'].ux },
+];
+export default RECOMMEND_BOOKS_LIST;
