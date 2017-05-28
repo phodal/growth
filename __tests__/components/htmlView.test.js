f71b251165dec993bf3ae82565cba82e5cd84b2d
travelgeezer <travelgeezer@icloud.com>
2017-05-28 16:26:22 +0800

    [T] test: add test html view component

---
+++ __tests__/components/htmlView.test.js
@@ -0,0 +1,21 @@
+import 'react-native';
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import HtmlView from '../../src/components/HtmlView';
+import HELPER_ARTICLES from '../../src/constants/HELPER_ARTICLES';
+
+jest.mock('react-native-fs', () => ({
+  writeFile: jest.fn(() => Promise.resolve()),
+  ExternalStorageDirectoryPath: 'package-path',
+}));
+
+it('renders correctly', () => {
+  // eslint-disable-next-line no-unused-vars
+  const htmlView = renderer.create(
+    <HtmlView domain={'article'} slug={HELPER_ARTICLES['zh-cn'].hello[0].slug} />,
+  );
+
+  const htmlViewJson = htmlView.toJSON();
+  expect(htmlViewJson).toMatchSnapshot();
+});
