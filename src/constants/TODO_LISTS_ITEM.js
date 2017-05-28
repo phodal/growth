e6b2fc45aac412013a8f18ceed4f7a759338a53d
travelgeezer <travelgeezer@icloud.com>
2017-05-28 23:44:00 +0800

    [T] constants: create todo lists item

---
+++ src/constants/TODO_LISTS_ITEM.js
@@ -0,0 +1,21 @@
+import TODO_LISTS from './TODO_LISTS';
+
+const TODO_LIST = TODO_LISTS['zh-cn'];
+const ITEM = [
+  TODO_LIST.hello,
+  TODO_LIST.before,
+  TODO_LIST.frontend,
+  TODO_LIST.backend,
+  TODO_LIST.env,
+  TODO_LIST.test,
+  TODO_LIST.refactor,
+  TODO_LIST.configurable,
+  TODO_LIST.ci,
+  TODO_LIST.container,
+  TODO_LIST.retro,
+  TODO_LIST.server,
+  TODO_LIST.analytics,
+  TODO_LIST.arch,
+
+];
+export default ITEM;
