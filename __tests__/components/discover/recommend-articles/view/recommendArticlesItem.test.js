2781769d2ea981ef5535ab1f4dbea51c53236af2
travelgeezer <travelgeezer@icloud.com>
2017-05-28 12:02:52 +0800

    [T] test: update test recommend articles item

--- __tests__/components/discover/recommend-articles/view/recommendArticlesItem.test.js
+++ __tests__/components/discover/recommend-articles/view/recommendArticlesItem.test.js
@@ -1,13 +1,13 @@
 import 'react-native';
 import React, {} from 'react';
 import renderer from 'react-test-renderer';
-import RecommendArticlesItem from '../../../../../src/components/discover/recommend-articles/view/RecommendArticlesItem';
+import RecommendSimpleItem from '../../../../../src/components/discover/recommend-articles/view/RecommendSimpleItem';
 import RECOMMEND_ARTICLES_LIST from '../../../../../src/constants/RECOMMEND_ARTICLES_LIST';
 
 it('renders correctly', () => {
-  const recommendArticlesItem = renderer.create(
-    <RecommendArticlesItem content={RECOMMEND_ARTICLES_LIST[0]} />,
+  const recommendSimpleItem = renderer.create(
+    <RecommendSimpleItem content={RECOMMEND_ARTICLES_LIST[0]} />,
   );
-  const recommendArticlesItemJson = recommendArticlesItem.toJSON();
-  expect(recommendArticlesItemJson).toMatchSnapshot();
+  const recommendSimpleItemJson = recommendSimpleItem.toJSON();
+  expect(recommendSimpleItemJson).toMatchSnapshot();
 });
