3e1f51297936cdd32e9d7608556207ef35874d97
Phodal HUANG <h@phodal.com>
2017-06-01 17:02:03 +0800

    [Home] SectionPage: update article layout

--- src/containers/home/sections/SectionPage.js
+++ src/containers/home/sections/SectionPage.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
-import { ListItem } from 'react-native-elements';
+import { List, ListItem } from 'react-native-elements';
 import * as shortid from 'shortid';
 
 import SECTIONS from '../../../constants/SECTIONS';
@@ -43,20 +43,32 @@ class SectionPage extends Component {
   renderHeader = props => <TabBar {...props} />;
 
   renderScene = SceneMap({
-    1: () => (
-      <View>
-        <Text>了解优秀软件工程实践所需的信息</Text>
-        {
-          this.state.sectionInfo.articlesView ?
-            this.state.sectionInfo.articlesView[0].articles.map(article => (
-              <ListItem
-                key={shortid.generate()}
-                title={article.title}
-              />
-            )) : ''
-        }
-      </View>
-    ),
+    1: () => {
+      const articlesView = this.state.sectionInfo.articlesView;
+
+      if (!articlesView) {
+        return <Text />;
+      }
+
+      const articleViewComponent = articlesView[0].articles.map(article => (
+        <ListItem
+          key={shortid.generate()}
+          title={article.title}
+        />
+      ));
+
+      return (
+        <View>
+          <View style={{ backgroundColor: '#2d89ef' }}>
+            <Text style={{ margin: 10, color: '#fff', textAlign: 'center' }}>了解优秀软件工程实践所需的信息</Text>
+          </View>
+          <List>
+            {articleViewComponent}
+          </List>
+        </View>
+      );
+    },
+
     2: () => (
       <View>
         <Text>扩展自己的知识视野</Text>
