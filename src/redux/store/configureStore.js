b643c3f5128796e2c4700492689cbc066c710783
Phodal Huang <h@phodal.com>
2017-06-02 16:28:24 +0800

    [T] fix redux log  issue

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,24 +1,29 @@
 /* eslint-disable global-require,no-unused-vars */
 import { createStore, applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import reducers from '../routes/index';
 
-const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+const loggerMiddleware = createLogger({
+  predicate: (getState, action) => __DEV__,
+});
 
 export default function configureStore() {
-  const enhancer = compose(
-    applyMiddleware(
-      loggerMiddleware,
-    ),
-  );
-  const store = createStore(reducers, enhancer);
+  let middleware = [
+    thunk,
+  ];
 
-  if (module.hot) {
-    module.hot.accept(() => {
-      const nextRootReducer = require('../routes/index').default;
-      store.replaceReducer(nextRootReducer);
-    });
+  if (__DEV__) {
+    // Dev-only middleware
+    middleware = [
+      ...middleware,
+      loggerMiddleware,
+    ];
   }
-  return store;
+
+  const enhancer = compose(
+    applyMiddleware(...middleware),
+  );
+  return createStore(reducers, enhancer);
 }
