a63764f7972528072581ff2730e6efcb9b791220
Phodal Huang <h@phodal.com>
2017-06-02 17:14:58 +0800

    [T] add redux-mock to pass test

--- __tests__/containers/home/sectionPage.test.js
+++ __tests__/containers/home/sectionPage.test.js
@@ -1,12 +1,20 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
 
 import SectionPage from '../../../src/containers/home/sections/SectionPage';
 
-xit('renders correctly', () => {
+const mockStore = configureStore();
+const dispatch = () => {};
+
+it('renders correctly', () => {
   const tree = renderer.create(
-    <SectionPage sectionIndex={1} subSectionIndex={1} />,
+    <SectionPage
+      dispatch={dispatch}
+      store={mockStore({ section: {} })}
+      sectionIndex={1} subSectionIndex={1}
+    />,
   );
 
   const treeJson = tree.toJSON();
