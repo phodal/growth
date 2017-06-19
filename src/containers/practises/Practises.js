e5c862983091126ed6e5697ee14a1cb72a620d18
Phodal Huang <h@phodal.com>
2017-06-19 09:37:50 +0800

    [T] remove unused code

--- src/containers/practises/Practises.js
+++ src/containers/practises/Practises.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { ListItem, List } from 'react-native-elements';
-// import FileHelper from '../../utils/FileHelper';
 
 class Practises extends Component {
   static componentName = 'Practises';
 
   render() {
-    // FileHelper.fetchFile('https://github.com/phodal/growth-leetcode-api/archive/master.zip', 'leetcode');
-
     return (
       <ScrollView>
         <List>
