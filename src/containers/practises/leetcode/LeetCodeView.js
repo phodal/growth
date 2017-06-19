0a2ab33db975e8c6296aef5f85b078a3eb0a2e7e
Phodal Huang <h@phodal.com>
2017-06-19 10:49:40 +0800

    [LeetCode] inline popup code

--- src/containers/practises/leetcode/LeetCodeView.js
+++ src/containers/practises/leetcode/LeetCodeView.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
+  StyleSheet,
   Dimensions,
 } from 'react-native';
 import Carousel from 'react-native-looped-carousel';
+import Modal from 'react-native-modalbox';
+import RNFetchBlob from 'react-native-fetch-blob';
+import Toast from 'react-native-simple-toast';
+import * as Progress from 'react-native-progress';
+
 import AsyncStorageHelper from '../../../utils/AsyncStorageHelper';
-import FetchFileModal from '../../../components/helper/FetchFileModal';
 
 const randomColor = require('randomcolor'); // import the script
-
 const { width, height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingTop: 50,
+    flex: 1,
+  },
+
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 300,
+    width: 300,
+  },
+});
 
 function getRandomColor() {
   return randomColor({
@@ -32,11 +49,15 @@ export default class LeetCodeView extends Component {
   }
 
   componentWillMount() {
+    const self = this;
     AsyncStorageHelper.get('leetcode.downloaded', (err, result) => {
       if (result === 'true') {
-        this.setState({
+        self.setState({
           hasDownloaded: true,
         });
+      } else {
+        self.modal.open();
+        self.fetchFile('https://github.com/phodal/growth-leetcode-api/archive/master.zip', 'leetcode');
       }
     });
   }
@@ -46,14 +67,51 @@ export default class LeetCodeView extends Component {
     this.setState({ size: { width: layout.width, height: layout.height } });
   };
 
+  fetchFile(url, fileName) {
+    const DIR = RNFetchBlob.fs.dirs;
+
+    RNFetchBlob
+    .config({
+      path: `${DIR.DocumentDir}/${fileName}.zip`,
+    })
+    .fetch('GET', url, {
+      // some headers ..
+    })
+    .progress((received, total) => {
+      let progress = received / total;
+      if (progress > 1) {
+        progress = 1;
+      }
+      this.setState({ progress });
+    })
+    .then((res) => {
+      const now = new Date();
+      console.log(`${now} 下载成功`);
+      console.log(res.path());
+      AsyncStorageHelper.set('leetcode.downloaded', 'true');
+      this.setState({ hasDownloaded: true });
+    })
+    .catch((err) => {
+      Toast.show(`下载失败 ${err}`);
+    });
+  }
+
   render() {
     const hasDownloaded = this.state.hasDownloaded;
     if (!hasDownloaded) {
       return (
-        <FetchFileModal
-          url={'https://github.com/phodal/growth-leetcode-api/archive/master.zip'}
-          fileName={'leetcode'}
-        />
+        <Modal
+          style={[styles.modal]}
+          position={'center'}
+          ref={(modal) => { this.modal = modal; }}
+        >
+          <Progress.Circle
+            style={styles.progress}
+            progress={this.state.progress}
+            indeterminate
+            size={80}
+          />
+        </Modal>
       );
     }
 
