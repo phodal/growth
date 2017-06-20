dfd170460a7b3e8a100de304ad6bac3565c46410
Fengda HUANG <h@phodal.com>
2017-06-20 21:38:04 +0800

    [T] fix react issue

--- src/components/TabIcon.js
+++ src/components/TabIcon.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
 import AppColors from '../theme/colors';
--- src/containers/discover/Discover.js
+++ src/containers/discover/Discover.js
@@ -21,7 +21,7 @@ class Discover extends Component {
         </View>
         <View style={AppStyle.discoverFirstGrid}>
           <GridItem title="在线电子书" iconName="icon-social" position={'left'} onclick={() => { Launch.freeBookList(); }} />
-          <GridItem title="Awesome" iconName="md-book" position={'right'} onclick={() => { Launch.awesomeLists(); }} />
+          <GridItem title="Awesome" iconName="icon-graphic" position={'right'} onclick={() => { Launch.awesomeLists(); }} />
         </View>
         <View style={AppStyle.discoverOtherGrid}>
           <GridItem title="学习路线" iconName="icon-social" position={'left'} onclick={() => { Launch.roadmapList(); }} />
