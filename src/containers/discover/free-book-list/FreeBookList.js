b600f04f7214848c1b336c245168db705641986f
Phodal Huang <h@phodal.com>
2017-06-21 11:04:20 +0800

    [Discover] FreeBookList: simplify code

--- src/containers/discover/free-book-list/FreeBookList.js
+++ src/containers/discover/free-book-list/FreeBookList.js
@@ -57,33 +57,31 @@ class FreeBookList extends Component {
     }
 
     return (
-      <ScrollView>
-        <View containerStyle={{ marginBottom: 20 }}>
-          {
-            rowData.map(sections => (
-              <View
-                key={shortid.generate()}
-              >
-                <View style={{ padding: 10 }}><Text style={{ textAlign: 'center' }}>{sections.heading}</Text></View>
-                <FlatList
-                  data={sections.childrens}
-                  renderItem={({ item }) => (
-                    <TouchableHighlight
-                      onPress={() => Helper.openLink(item.href)}
-                    >
-                      <View style={{ backgroundColor: 'white' }}>
-                        <ListItem
-                          title={item.title}
-                          subtitle={`类型：${item.type}`}
-                        />
-                      </View>
-                    </TouchableHighlight>
-                  )}
-                />
-              </View>
-            ))
-          }
-        </View>
+      <ScrollView containerStyle={{ marginBottom: 20 }}>
+        {
+          rowData.map(sections => (
+            <View
+              key={shortid.generate()}
+            >
+              <View style={{ padding: 10 }}><Text style={{ textAlign: 'center' }}>{sections.heading}</Text></View>
+              <FlatList
+                data={sections.childrens}
+                renderItem={({ item }) => (
+                  <TouchableHighlight
+                    onPress={() => Helper.openLink(item.href)}
+                  >
+                    <View style={{ backgroundColor: 'white' }}>
+                      <ListItem
+                        title={item.title}
+                        subtitle={`类型：${item.type}`}
+                      />
+                    </View>
+                  </TouchableHighlight>
+                )}
+              />
+            </View>
+          ))
+        }
       </ScrollView>
     );
   }
