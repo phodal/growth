5b3de92a5b15419d0b18fc83056b414848ae71e4
Phodal Huang <h@phodal.com>
2017-06-22 17:55:18 +0800

    [Home] update UI

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars,global-require */
 import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import Search from 'react-native-search-box';
-import { Button, List, ListItem } from 'react-native-elements';
+import { Button, Card, List, ListItem } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 
 class Home extends Component {
@@ -49,7 +49,7 @@ class Home extends Component {
 
   render() {
     const homeView = (
-      <View>
+      <View style={{ paddingBottom: 20 }}>
         <View>
           <Button
             icon={{ name: 'exit-to-app' }}
@@ -69,26 +69,44 @@ class Home extends Component {
         </View>
 
         <View style={{ paddingTop: 20 }}>
-          <Text>为您推荐</Text>
-          <List>
-            <ListItem
-              title="example"
+          <Text style={{ paddingLeft: 20 }}>为您推荐</Text>
+          <Card
+            title="Awesome"
+            image={require('../../../assets/growth-ui/img/home-4.jpg')}
+          >
+            <Text style={{ marginBottom: 10 }}>
+              Awesome J  fsdf dsf sf sdfd afd
+            </Text>
+            <Button
+              icon={{ name: 'code' }}
+              backgroundColor="#03A9F4"
+              buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+              title="查看"
             />
-          </List>
+          </Card>
         </View>
         <View style={{ paddingTop: 20 }}>
-          <Text>今日精选</Text>
-          <List>
-            <ListItem
-              title="example"
+          <Text style={{ paddingLeft: 20 }}>今日精选</Text>
+          <Card
+            title="LeetCode"
+            image={require('../../../assets/growth-ui/img/home-5.jpg')}
+          >
+            <Text style={{ marginBottom: 10 }}>
+              leetcode J  fsdf dsf sf sdfd afd
+            </Text>
+            <Button
+              icon={{ name: 'code' }}
+              backgroundColor="#03A9F4"
+              buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+              title="查看"
             />
-          </List>
+          </Card>
         </View>
       </View>
       );
 
     return (
-      <ScrollView>
+      <ScrollView style={{ paddingBottom: 20 }}>
         <View style={{ paddingTop: 5, paddingBottom: 5 }}>
           <Search
             ref={searchBox => (this.searchBox = searchBox)}
