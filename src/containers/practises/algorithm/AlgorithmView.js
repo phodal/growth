f2a7e81b8e0be877fcba8004f3308dd6282aa79e
Phodal Huang <h@phodal.com>
2017-06-28 15:53:36 +0800

    [Practise] Algorithm: create category

--- src/containers/practises/algorithm/AlgorithmView.js
+++ src/containers/practises/algorithm/AlgorithmView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import { ListItem, List } from 'react-native-elements';
+import Helper from '../../../utils/helper';
+import Label from '../../../components/Label';
 
 class AlgorithmView extends Component {
   static componentName = 'AlgorithmView';
@@ -8,6 +10,15 @@ class AlgorithmView extends Component {
   render() {
     return (
       <ScrollView>
+        <Label text="资源推荐" />
+        <List containerStyle={{ borderTopWidth: 0 }}>
+          <ListItem
+            title={'VisuAlgo'}
+            onPress={() => Helper.openLink('https://visualgo.net/zh')}
+          />
+        </List>
+
+        <Label text="图说" />
         <List containerStyle={{ borderTopWidth: 0 }}>
           <ListItem
             title={'快速排序'}
