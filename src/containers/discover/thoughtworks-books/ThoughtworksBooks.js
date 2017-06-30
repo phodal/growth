01cce444336a2795c11872e8229b136e02c47d4d
Phodal HUANG <h@phodal.com>
2017-06-30 13:20:56 +0800

    [Discover] refactor-list-view: tw books

--- src/containers/discover/thoughtworks-books/ThoughtworksBooks.js
+++ src/containers/discover/thoughtworks-books/ThoughtworksBooks.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import SimpleListItem from '../../../components/discover/view/SimpleListItem';
+import { FlatList, ScrollView } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import AppStyle from '../../../theme/styles';
 import Launch from '../../../components/discover/Launch';
 
@@ -14,17 +14,29 @@ const BOOKS = [
 class ThoughtworksBooks extends Component {
   static componentName = 'ThoughtworksBooks';
 
-  render() {
-    const rows = Array.from(new Array(BOOKS.length))
-      .map((val, index) => (
-        <SimpleListItem
-          text={BOOKS[index].name}
-          click={() => Launch.domainDetailPage(BOOKS[index].name, BOOKS[index].path)}
-          key={BOOKS[index].name.concat(index)}
-        />));
 
+  keyExtractor = (item, index) => `key${index}`;
+
+// eslint-disable-next-line no-unused-vars
+  renderList = ({ item, index }) => (
+    <ListItem
+      title={BOOKS[index].name}
+      key={BOOKS[index].name.concat(index)}
+      onPress={() => Launch.domainDetailPage(BOOKS[index].name, BOOKS[index].path)}
+    />
+  );
+
+  render() {
     return (
-      <ScrollView style={AppStyle.detailBasisStyle}>{rows}</ScrollView>
+      <ScrollView style={AppStyle.detailBasisStyle}>
+        <List containerStyle={{ borderTopWidth: 0, marginTop: 0 }}>
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={BOOKS}
+            renderItem={this.renderList}
+          />
+        </List>
+      </ScrollView>
     );
   }
 }
