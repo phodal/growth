9247ef9f57339f629958f866bf39ff834500409c
Phodal Huang <h@phodal.com>
2017-07-04 15:41:13 +0800

    Revert "[Discover] add relate info"
    This reverts commit 07f1db5f6cafebb3f4907645a18fc37f7aaafc0b.

--- src/containers/practises/moregex/MoRegexView.js
+++ src/containers/practises/moregex/MoRegexView.js
@@ -1,63 +1,27 @@
 /* eslint-disable global-require,no-unused-vars,class-methods-use-this */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Dimensions, View, WebView, StyleSheet, Platform, Text, Animated } from 'react-native';
+import { Dimensions, View, WebView, StyleSheet, Platform } from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
-import Interactable from 'react-native-interactable';
-import { Button } from 'react-native-elements';
 
 import AppSizes from '../../../theme/sizes';
 
 
-const Screen = {
-  width: Dimensions.get('window').width,
-  height: Dimensions.get('window').height - 40,
-};
-
 const styles = StyleSheet.create({
-  panelContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-  panel: {
-    height: Screen.height + 300,
-    padding: 20,
-    backgroundColor: '#f7f5eee8',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowRadius: 5,
-    shadowOpacity: 0.4,
-  },
-  panelHeader: {
-    alignItems: 'center',
-  },
-  panelHandle: {
-    width: 40,
-    height: 8,
-    borderRadius: 4,
-    backgroundColor: '#00000040',
-    marginBottom: 10,
-  },
-  panelTitle: {
-    fontSize: 20,
-    height: 35,
-  },
-  panelSubtitle: {
-    fontSize: 14,
-    color: 'gray',
-    height: 30,
-    marginBottom: 10,
+  container: {
+
   },
   background: {
     backgroundColor: 'transparent',
     height: AppSizes.screen.height,
     width: AppSizes.screen.width,
+  },
+  logo: {
+    width: AppSizes.screen.width * 0.85,
+    resizeMode: 'contain',
   },
   whiteText: {
     color: '#FFF',
@@ -90,7 +54,6 @@ class MoRegexView extends Component {
   constructor() {
     super();
     this.webview = null;
-    this.deltaY = new Animated.Value(Screen.height - 100);
   }
 
   componentWillReceiveProps() {
@@ -128,39 +91,11 @@ class MoRegexView extends Component {
           automaticallyAdjustContentInsets={false}
           style={[styles.container, { height: Dimensions.get('window').height, width: webViewWidth }]}
         />
-        <View style={styles.panelContainer}>
-          <Animated.View
-            style={[styles.panelContainer, {
-              backgroundColor: 'black',
-              opacity: this.deltaY.interpolate({
-                inputRange: [0, Screen.height - 100],
-                outputRange: [0.5, 0],
-                extrapolateRight: 'clamp',
-              }),
-            }]}
-          />
-          <Interactable.View
-            verticalOnly
-            snapPoints={[{ y: 40 }, { y: Screen.height - 200 }, { y: Screen.height - 100 }]}
-            boundaries={{ top: - 200 }}
-            initialPosition={{ y: Screen.height - 100 }}
-            animatedValueY={this.deltaY}
-          >
-            <View style={styles.panel}>
-              <View style={styles.panelHeader}>
-                <View style={styles.panelHandle} />
-              </View>
-              <Text style={styles.panelTitle}>相关资料</Text>
-              <Text style={styles.panelSubtitle}>精选正则表达式资源</Text>
-              <Button
-                title={'MDN 正则表达式表'}
-                borderRadius={6}
-                backgroundColor={'#03a9f4'}
-                onPress={() => Actions.moRegexInfoView()}
-              />
-            </View>
-          </Interactable.View>
-        </View>
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor="#9b59b6" title="资料" onPress={() => Actions.moRegexInfoView()}>
+            <Icon name="md-information" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     );
   }
