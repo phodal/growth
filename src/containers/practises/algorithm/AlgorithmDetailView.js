7f9f37236a560eff681bb647d9b54f1203706a05
Phodal Huang <h@phodal.com>
2017-07-06 10:33:05 +0800

    [Practise] algorithm: add tabbar

--- src/containers/practises/algorithm/AlgorithmDetailView.js
+++ src/containers/practises/algorithm/AlgorithmDetailView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import {
+  StyleSheet, Text, View, ScrollView, TouchableOpacity,
+} from 'react-native';
 import Swiper from 'react-native-swiper';
 
 
@@ -16,6 +18,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  tabar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 44,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
 
@@ -36,20 +50,41 @@ class AlgorithmDetailView extends Component {
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
-        <Text style={{ padding: 20, textAlign: 'center' }}> 这个功能在 TODO List 里，如果你需要请点个赞</Text>
+        <View>
+          <Text style={{ padding: 20, textAlign: 'center' }}> 这个功能在 TODO List 里，如果你需要请点个赞</Text>
+        </View>
 
-        <Swiper style={styles.wrapper}>
-          <ScrollView contentContainerStyle={styles.slide}>
-            <Text style={styles.text}>Hello Swiper</Text>
-          </ScrollView>
-          <ScrollView contentContainerStyle={styles.slide}>
-            <Text style={styles.text}>Beautiful</Text>
-          </ScrollView>
-          <ScrollView contentContainerStyle={styles.slide}>
-            <Text style={styles.text}>And simple</Text>
-          </ScrollView>
-        </Swiper>
+        <View style={{ height: 200 }}>
+          <Swiper style={styles.wrapper}>
+            <ScrollView contentContainerStyle={styles.slide}>
+              <Text style={styles.text}>Hello Swiper</Text>
+            </ScrollView>
+            <ScrollView contentContainerStyle={styles.slide}>
+              <Text style={styles.text}>Beautiful</Text>
+            </ScrollView>
+            <ScrollView contentContainerStyle={styles.slide}>
+              <Text style={styles.text}>And simple</Text>
+            </ScrollView>
+          </Swiper>
+        </View>
 
+        <View style={styles.tabar}>
+          <TouchableOpacity>
+            <View>
+              <Text>Detail</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View>
+              <Text>Detail</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View>
+              <Text>Detail</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
