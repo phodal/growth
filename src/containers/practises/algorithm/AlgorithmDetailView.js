dbd9273ec4356971c6341b6c03dbbd6bf1ec5549
Phodal Huang <h@phodal.com>
2017-07-06 10:08:50 +0800

    [Practise] algorithm: add slide

--- src/containers/practises/algorithm/AlgorithmDetailView.js
+++ src/containers/practises/algorithm/AlgorithmDetailView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Tabs, Tab, Icon } from 'react-native-elements';
+import Swiper from 'react-native-swiper';
 
 
 const styles = StyleSheet.create({
@@ -9,6 +9,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 12,
+  },
+  wrapper: {
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
@@ -26,58 +33,22 @@ class AlgorithmDetailView extends Component {
     };
   }
 
-  changeTab(selectedTab) {
-    this.setState({ selectedTab });
-  }
-
   render() {
-    const { selectedTab } = this.state;
-
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
         <Text style={{ padding: 20, textAlign: 'center' }}> 这个功能在 TODO List 里，如果你需要请点个赞</Text>
 
-        <Tabs>
-          <Tab
-            titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-            selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-            selected={selectedTab === 'detail'}
-            title={selectedTab === 'detail' ? 'DETAIL' : null}
-            renderIcon={() => <Icon containerStyle={styles.tabbar} color={'#5e6977'} name="device-hub" size={33} />}
-            renderSelectedIcon={() => <Icon color={'#6296f9'} name="device-hub" size={30} />}
-            onPress={() => this.changeTab('detail')}
-          >
-            <ScrollView>
-              <Text>DETAIL</Text>
-            </ScrollView>
-          </Tab>
-          <Tab
-            titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-            selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-            selected={selectedTab === 'execution'}
-            title={selectedTab === 'execution' ? 'EXECUTION' : null}
-            renderIcon={() => <Icon containerStyle={styles.tabbar} color={'#5e6977'} name="assessment" size={33} />}
-            renderSelectedIcon={() => <Icon color={'#6296f9'} name="assessment" size={30} />}
-            onPress={() => this.changeTab('execution')}
-          >
-            <ScrollView>
-              <Text>DETAIL</Text>
-            </ScrollView>
-          </Tab>
-          <Tab
-            titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-            selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-            selected={selectedTab === 'code'}
-            title={selectedTab === 'code' ? 'CODE' : null}
-            renderIcon={() => <Icon containerStyle={styles.tabbar} color={'#5e6977'} name="code" size={33} />}
-            renderSelectedIcon={() => <Icon color={'#6296f9'} name="code" size={30} />}
-            onPress={() => this.changeTab('code')}
-          >
-            <ScrollView>
-              <Text>CODE</Text>
-            </ScrollView>
-          </Tab>
-        </Tabs>
+        <Swiper style={styles.wrapper}>
+          <ScrollView contentContainerStyle={styles.slide}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </ScrollView>
+          <ScrollView contentContainerStyle={styles.slide}>
+            <Text style={styles.text}>Beautiful</Text>
+          </ScrollView>
+          <ScrollView contentContainerStyle={styles.slide}>
+            <Text style={styles.text}>And simple</Text>
+          </ScrollView>
+        </Swiper>
 
       </View>
     );
