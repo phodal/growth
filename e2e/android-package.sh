2af3abfe1efd071676408bb702494c0d65bda0d2
Phodal Huang <h@phodal.com>
2017-07-17 10:22:51 +0800

    [T] e2e: update pacakge script

--- e2e/android-package.sh
+++ e2e/android-package.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+rm -rf android/app/src/main/assets/skilltree
+rm -rf android/app/src/main/assets/algorithm-webview
+rm -rf android/app/src/main/assets/moregex
+rm -rf android/app/src/main/assets/GrEditor
+
 cp -a src/containers/skill-tree/www android/app/src/main/assets/skilltree
 cp -a src/containers/practises/algorithm/algorithm-webview android/app/src/main/assets/algorithm-webview
 cp -a src/containers/practises/moregex/www android/app/src/main/assets/moregex
